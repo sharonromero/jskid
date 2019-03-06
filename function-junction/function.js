e50ea8d321f7c9ddf14001a09f0c673f46df1a43
sharonromero <sh-aron@hotmail.com>
2019-03-05 17:38:35 -0700

    Changed some css, added js Todos

---
+++ function-junction/function.js
@@ -0,0 +1,43 @@
+/*
+todo: create three global variables:
+* trainSpeed (initial value = 250)
+* trainPosition (initial value = 0)
+* animation (no initial value)
+*/
+
+/*
+todo: listen for click events on the train element and call a function named speedUp when they happen. 
+*/
+
+/*
+todo: listen for click events on the stop button element and call a function called stopTrain when they happen. 
+*/
+
+function speedUp() {
+    /* 
+    todo: check whether the train is already going as fast as it can. If not, increase the speed.
+    */
+    
+    /*
+    If the train is already moving, stop it and then restart with the new speed by calling a function called frame.
+    */
+
+
+    function frame() {
+        /* 
+        reposition the train and check whether the train is crashed
+        */
+    }
+}
+
+function checkPosition(currentPosition) {
+    /*
+    todo: check the train's current position and crash it if it's at the end of the line
+    */
+}
+
+function stopTrain() {
+    /* 
+    todo: test whether the train is already crashed. If not, stop the train. 
+    */
+}
