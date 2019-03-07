af0cd22360b3dc91893c2f3b92b8b084f7b70a5f
sharonromero <sh-aron@hotmail.com>
2019-03-06 17:47:02 -0700

    Updated index.html & css, created functions

--- function-junction/function.js
+++ function-junction/function.js
@@ -4,29 +4,41 @@ todo: create three global variables:
 * trainPosition (initial value = 0)
 * animation (no initial value)
 */
+var trainSpeed = 260;
+var trainPosition = 0;
+var animation;
 
 /*
 todo: listen for click events on the train element and call a function named speedUp when they happen. 
 */
+var train = document.getElementById("train");
+train.addEventListener("click", speedUp);
 
 /*
 todo: listen for click events on the stop button element and call a function called stopTrain when they happen. 
 */
+var stopButton = document.getElementById("stopButton");
+stopButton.addEventListener("click", stopTrain);
 
 function speedUp() {
     /* 
     todo: check whether the train is already going as fast as it can. If not, increase the speed.
     */
-    
+    if (trainSpeed > 10) {
+        trainSpeed -= 10;
+        clearInterval(animation);
+        animation = setInterval(frame, trainSpeed);   
+    }
     /*
     If the train is already moving, stop it and then restart with the new speed by calling a function called frame.
     */
-
-
     function frame() {
         /* 
         reposition the train and check whether the train is crashed
         */
+        trainPosition += 2;
+        train.style.left = trainPosition + 'px';
+        checkPosition(trainPosition);
     }
 }
 
@@ -34,10 +46,18 @@ function checkPosition(currentPosition) {
     /*
     todo: check the train's current position and crash it if it's at the end of the line
     */
+    if (currentPosition === 260) {
+        alert("Crash!");
+        console.log("Crash!");
+        clearInterval (animation);
+    }
 }
 
 function stopTrain() {
     /* 
     todo: test whether the train is already crashed. If not, stop the train. 
     */
-}
+    if (trainPosition < 260) {
+        clearInterval (animation);
+    }
+  }  
