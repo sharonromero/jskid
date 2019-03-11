4ed505d46bbde6d28ca1eec7ec7475bf7b6343de
sharonromero <sh-aron@hotmail.com>
2019-03-11 14:35:53 -0600

    Added html list item and buttons for form

---
+++ wish-list/wish.js
@@ -0,0 +1,42 @@
+//create an event listener for the print button, with a handler function called printView
+
+// var printButton = 
+
+//create an event listener for the add button, with a handler function called addTheThing
+
+// var addButton = 
+
+// create a blank array named myList
+
+/* create a variable, myListArea, which references the element with the id of 'wishList'
+
+// var myListArea = 
+
+/* function addTheThing gets the value of the text field and then passes it to a function called addToTheList. It then runs a function called resetInput
+
+function addTheThing() {
+
+
+}
+*/
+/* function addToTheList, which takes one parameter, 
+called thingToAdd, pushes it into the myList array, and then 
+adds it to myListArea
+
+function addToTheList(thingToAdd) {
+    
+
+}
+*/
+/* function resetInput, which resets the value of the
+input field to blank ("")
+function resetInput(inputToReset) {
+
+}
+*/
+/*function printView, which outputs a nicely formatted 
+view of the list 
+function printView() {
+  
+}
+*/
