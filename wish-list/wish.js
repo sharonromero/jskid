5ebb971dd999cbb472cecc9b23cbbd93f4a55de5
sharonromero <sh-aron@hotmail.com>
2019-03-11 16:17:16 -0600

    Wrote addToTheList() function

--- wish-list/wish.js
+++ wish-list/wish.js
@@ -33,23 +33,33 @@ addToTheList(theThing);
 resetInput(theThing);
 }
 
-// function addToTheList, which takes one parameter, 
-called thingToAdd, pushes it into the myList array, and then 
-adds it to myListArea
+// addToTheList function takes one parameter called thingToAdd, pushes it into the myList array, and then adds it to myListArea.
+// myListArea is the unordered list in the browser window.
 
 function addToTheList(thingToAdd) {
-    
+    myList.push(thingToAdd.value); // This statement uses the push() array method to add the current value of the input field to the myList array. The push() array method adds value to the end of an array.
+    var newListItem = document.createElement("li"); // The createElement() method causes a new element to be created in the current browser window.
+	
+	// The next statement changes the innerHTML property of the new element to put the current value of the input field into it.
+	// Each variable inside the statement is a stand-in for something else.
+	// The expression returns the length of the myList array and then subtracts 1 from it. We want to find out the index number of the last element of the array (the element we just added.)
+	// We subtract 1 from the length of the array because arrays start counting at 0.
+    newListItem.innerHTML = myList[myList.length -1];
 
+    // The appendChild() method adds our brand new <li> element to the end of the contents of the element referenced by the myListArea variable.
+    // The statement below adds a new list item to the <ul> element, which displays the new item in the browser window.
+    // The myListArea global variable was created on line 20 to hold a reference to the <ul> element with an ID attribute of "wishList".
+    myListArea.appendChild(newListItem);
 }
 
 // function resetInput, which resets the value of the
-input field to blank ("")
-function resetInput(inputToReset) {
+// input field to blank ("")
+// function resetInput(inputToReset) {
 
-}
+// }
 
 // function printView, which outputs a nicely formatted 
-view of the list 
+// view of the list 
 function printView() {
   
 }
