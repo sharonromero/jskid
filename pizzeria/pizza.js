8b82a6d9759444b0142b8cc86f71c9f327320fe9
sharonromero <sh-aron@hotmail.com>
2019-03-13 16:25:40 -0600

    Updated index.html, pizza.css, & pizza.js
    Added styles to index.html, added extra delivery for supreme pizza and delivery outside of Anytown, added free delivery for birthday, and markup for supreme pizza, new cities, and birthday.

--- pizzeria/pizza.js
+++ pizzeria/pizza.js
@@ -1,4 +1,4 @@
-// listen for button clicks
+// listens for button clicks
 document.getElementById("placeOrder").addEventListener("click", placeOrder);
 
 /**
@@ -7,25 +7,31 @@ document.getElementById("placeOrder").addEventListener("click", placeOrder);
  * produces output
  */
 function placeOrder() {
-    // get form values
+    // gets form values
     var numPizzas = document.getElementById("numPizzas").value;
     var typePizza = document.getElementById("typePizza").value;
     var deliveryCity = document.getElementById("deliveryCity").value;
+    var birthday = document.getElementById("birthday").value;
 
-    // get the pizza price
+    // gets the pizza price
     var orderPrice = calculatePrice(numPizzas, typePizza);
 
-    // get the delivery price
-    var deliveryPrice = calculateDelivery(orderPrice, deliveryCity);
+    // gets the delivery price
+    var deliveryPrice = calculateDelivery(orderPrice, deliveryCity, birthday);
 
-    // create the output
+    // creates the output
     var theOutput = "<p>Thank you for your order.</p>";
 
-    // todo: output the delivery price, if there is one
+    // Outputs the delivery price, if there is one. Prints out a free delivery message if the deliveryPrice is 0, otherwise, it prints out the delivery charge.
+    if (deliveryPrice === 0) {
+        theOutput += "<p>You get free delivery!</p>";
+    } else {
+        theOutput += "<p>Your delivery cost is: $" + deliveryPrice;
+    }
 
     theOutput += "<p>Your total is: $" + (orderPrice + deliveryPrice);
 
-    // display the output
+    // displays the output
     document.getElementById("displayTotal").innerHTML = theOutput;
 }
 
@@ -36,7 +42,11 @@ function calculatePrice(numPizzas, typePizza) {
     var orderPrice = Number(numPizzas) * 10;
     var extraCharge = 0;
     
-    // todo: calculate extraCharge, if there is one.
+    // Calculates extraCharge, if there is one. It checks the typePizza variable to see if the Supreme pizza was selected.
+    // If the Supreme pizza was selected it will multiply the number of pizzas by two in order to get the number of dollars to add to the price.
+    if (typePizza === "supreme") {
+        extraCharge = Number(numPizzas) * 2;
+    }
     
     orderPrice += extraCharge;
     return orderPrice;
@@ -45,10 +55,14 @@ function calculatePrice(numPizzas, typePizza) {
 /**
  * calculates delivery price
  */
-function calculateDelivery(orderPrice, deliveryCity) {
+function calculateDelivery(orderPrice, deliveryCity, birthday) {
     var deliveryPrice = 0;
     
-    // todo: calculate delivery price, if there is one
-    
+    // Calculates delivery price, if there is one
+    if (((deliveryCity === "Anytown") && (orderPrice > 10)) || (birthday === "yes")) {
+        deliveryPrice = 0;
+    } else {
+        deliveryPrice = 5;
+    }
     return deliveryPrice;
 }
