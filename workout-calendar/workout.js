8a9839a1e7f38b11952ecc3f7b0e9fcfb5987ff7
sharonromero <sh-aron@hotmail.com>
2019-03-14 14:58:39 -0600

    Created index.html, workout.css, workout.js

---
+++ workout-calendar/workout.js
@@ -0,0 +1,24 @@
+var todayDate = document.getElementById("todaysdate");
+var todoButton = document.getElementById("whattodo");
+
+// add a listener to the whattodo button
+todoButton.addEventListener("click", displayActivity);
+
+// create a new Date object
+var d = new Date();
+
+// call the displayDate() function
+displayDate();
+
+function displayDate() {
+    // todo: display the current date in the todaysdate div
+}
+
+function displayActivity() {
+    // todo: find out the day of the week.
+
+    /* todo: set a variable, called youShould, with a different
+       string based on what day of the week it is. */
+    
+    // todo: output the value of youShould into the thingToDo div
+}
