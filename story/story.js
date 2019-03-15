4ca37be1083314e64f2d2af4aaff7b7237f5e8b7
sharonromero <sh-aron@hotmail.com>
2019-03-14 18:16:10 -0600

    Wrote first case for continueStory() function

--- story/story.js
+++ story/story.js
@@ -43,13 +43,51 @@ function askQuestion(questionNumber) {
 // getAnswer() gets the answer from the text field and pushes it into the answers array, then calls the continueStory function.
 function getAnswer() {
 	cleanInput = yourAnswer.value.toUpperCase(); // Gets the value from the input field and converts it to uppercase letters.
-	answer.push(cleanInput); // Uses the push array method to add the user's answer as a new element at the end of the answers array.
+	answers.push(cleanInput); // Uses the push array method to add the user's answer as a new element at the end of the answers array.
 	yourAnswer.value = ""; // Resets the input field, clearing the current value out of it.
 	continueStory(answers.length - 1); // Calls the continueStory() function, passing it the number of the last element in the answers array.
 }
 
-/* continueStory() displays part of the story or an error based on the value of an item in the answers array */
+// continueStory() displays part of the story or an error based on the value of an item in the answers array
+// continueStory() function uses if...else statements to determine whether the user entered a valid value and then to show the correct part of the story based on that input.
 function continueStory(answerNumber) {
+	// Using a switch statement to use the value of the argument to find out what question is being asked.
+	switch (answerNumber) {
+		// Says that if the user responded to the first question, run the if statement
+		case 0:
+		// Says if the first element in the array (which corresponds to the first question) is set to "YES", run the following statments.
+		if (answers[0] === "YES") {
+			// Gets the HTML from inside the div element with an ID of answer01 and overwrites the contents of the div with the ID of story.
+			// When you answer "Yes" to the question "Are you ready to begin the story?", the first part of the story will display.
+			story.innerHTML = document.getElementById("answer01").innerHTML;
+			// calls the askQuestion() function and tells it to ask question #1. This causes the askQuestion function to ask "Go to Mars, or stay home?".
+			askQuestion(1);
+			// If the user didn't answer "Yes", the else clause will run.
+			// Another if statement is put inside of the else clause so we can test for a value of "NO", but only if the answer wasn't "YES". 
+		} else if (answers[0] === "NO") {
+
+			// If the answer is "NO", the story div's innerHTML is set equal to the appropriate message.
+			story.innerHTML = document.getElementById("answer02").innerHTML;
+			// Since the user said they aren't ready to begin the story yet, ask them the first question again until they are ready.
+			askQuestion(0);
+		// Says to do the following if the user didn't enter Yes or No.	
+		} else {
+
+			// Sets the value of the story div to an error message, telling them to enter either Yes or No.
+			story.innerHTML = document.getElementById("err0").innerHTML;
+			// Asks the first question again.
+			askQuestion(0);
+		} 
+		break;
+		case 1:
+		break;
+		case 2:
+		break;
+		default:
+		break;
+
+	}
+
 }
 
 /* theEnd() ends the story and hides the input field */
