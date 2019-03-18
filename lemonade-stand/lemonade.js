6a932edede0bce195e40fc708b67ee8f2a5831e1
sharonromero <sh-aron@hotmail.com>
2019-03-18 16:47:08 -0600

    Created JS for lemonade stand

--- lemonade-stand/lemonade.js
+++ lemonade-stand/lemonade.js
@@ -1,101 +1,71 @@
-// create days of week array
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
-// define types of weather
 var weather = ["Sunny", "Partly Sunny", "Partly Cloudy", "Cloudy", "Raining", "Snowing", "Thunderstorm", "Foggy"];
 
-// set min and max temps
-var maxTemp = 110;
-var minTemp = 32;
+// Sets min and max temperatures
+var maxTemp = 100;
+var minTemp = 0;
 
-// cost (to you) of a cup of lemonade
-var lemonadeCost = 0.5;
+// Cost (to you) of a cup of lemonade
+var lemonadeCost = .25;
 
-// array for storing daily temps
+// Array for storing daily temps
 var dailyTemp = [];
 
-// listen for order
-document.getElementById("OpenTheStand").addEventListener("click", openTheStand);
-
-// make the week's weather
+// Calls the function when the program loads
 generateWeather();
 
-/**
-generates weather for the week
-**/
+// Generates weather for the week
 function generateWeather() {
-    var weatherToday;
-    var tempToday;
-    for (var i = 0; i < days.length; i++) {
-        weatherToday = weather[Math.floor(Math.random() * weather.length)];
-        tempToday = Math.floor(Math.random() * (maxTemp - minTemp) + minTemp);
-        dailyTemp[i] = tempToday;
-        document.getElementById("5DayWeather").innerHTML += "<div id='" + days[i] + "' class='" + weatherToday + "'><b>Forecast for " + days[i] + ":</b><br><br>" + weatherToday + " and " + tempToday + " degrees.</div>";
-    }
-}
+	// Variables to hold the current weather and temperature
+	var weatherToday;
+	var tempToday;
+	// Starts a for loop that will loop through each of the days of the week.
+	for (var i = 0; i < days.length; i++) {
+		// the weatherToday variable gets a random element from the weather array.
+		weatherToday = weather[Math.floor(Math.random() * weather.length)];
+		// Gets a random temperature between the values of the minTemp and maxTemp variables.
+		tempToday = Math.floor(Math.random() * (maxTemp - minTemp) + minTemp);
+		// Stores the temperature in the dailyTemp array.
+		dailyTemp[i] = tempToday;
+		// Outputs a message describing the day's weather.
+		document.getElementById("5DayWeather").innerHTML += "<div id='" + days[i] + "' class='" + weatherToday + "'><b>Forecast for " + days[i] + ":</b><br><br>" + weatherToday + " and " + tempToday + " degrees.</div>";
+		}
+	}
 
-/**
-calculates glasses of lemonade sold
-**/
+// Calculates glasses of lemonade sold.
 function openTheStand() {
-    var glassesSold = 0; // daily
-    var totalGlasses = 0; // weekly
-    var glassesLeft = 0; // left to sell
-
-    // clear out previous results
-    resetForm();
-
-    // get input
-    var numGlasses = Number(document.getElementById("numGlasses").value);
-    var glassPrice = Number(document.getElementById("glassPrice").value);
-
-
-    for (var i = 0; i < days.length; i++) {
-
-        // glasses sold depends on temp and price
-        glassesSold = Math.floor(dailyTemp[i] / glassPrice);
-
-        // how many glasses do we have now?
-        glassesLeft = numGlasses - totalGlasses;
-
-        // we can't sell more than we have
-        if (glassesSold > glassesLeft) {
-            glassesSold = glassesLeft;
-        }
-
-        // increase the weekly total
-        totalGlasses = glassesSold + totalGlasses;
-
-        // display daily total
-        document.getElementById("result").innerHTML += "<p>" + days[i] + ", you sold " + glassesSold + " glasses of lemonade.</p>";
-
-    }
-
-    displayResults(numGlasses, glassPrice, totalGlasses);
-
-}
-
-/**
-calculates results and displays a report
-**/
-function displayResults(weeklyInventory, glassPrice, weeklySales) {
-    // calculate results
-    var revenue = weeklySales * glassPrice;
-    var expense = weeklyInventory * lemonadeCost;
-    var leftOver = weeklyInventory - weeklySales;
-    var profit = revenue - expense;
-
-    // print out the weekly report
-    document.getElementById("result").innerHTML += "<p>You sold a total of " + weeklySales + " glasses of lemonade this week.</p>";
-    document.getElementById("result").innerHTML += "<p>Total revenue: $" + revenue + ".</p>";
-    document.getElementById("result").innerHTML += "<p>You have " + leftOver + " glasses of lemonade left over.</p>";
-    document.getElementById("result").innerHTML += "<p>Each glass costs you $" + lemonadeCost + ". Your profit was $" + profit + ".";
-}
-
-/**
-resets the game so that a new order can be placed
-**/
+	// Holds the daily number of glasses sold.
+	var glassesSold = 0;
+	// Holds the weekly total
+	var totalGlasses = 0;
+	// Holds the number of glasses left to sell.
+	var glassesLeft = 0;
+	// Clear previous results.
+	resetForm();
+	// Gets the values from the form fields.
+	var numGlasses = Number(document.getElementById("numGlasses").value);
+	var glassPrice = Number(document.getElementById("glassPrice").value);
+	// A new loop to cycle through each day of the week
+	for (var i = 0; i < days.length; i++) {
+		// Calculates the number of glasses sold.
+		// Glasses sold depends on temp and price.
+		glassesSold = Math.floor(dailyTemp[i] / glassPrice);
+		// Calculates how many glasses are left.
+		glassesLeft = numGlasses - totalGlasses;
+		// Checks whether you're out of lemonade.
+		// If glassesSold is greater than what we have left, sell what we have left by setting the glassesSold variable equal to what's left.
+		if (glassesSold > glassesLeft) {
+			glassesSold = glassesLeft;
+		}
+		// Increases the weekly total of glasses sold.
+		totalGlasses = glassesSold + totalGlasses;
+		// Display the daily totals.
+		document.getElementById("result").innerHTML += "<p>" + days[i] + ", you sold " + glassesSold + " glasses of lemonade.</p>";
+		}
+		displayResults(numGlasses, glassPrice, totalGlasses);
+	}
+// Resets the game so that a new order can be placed.
 function resetForm() {
-    document.getElementById("result").innerHTML = "";
-
+	document.getElementById("result").innerHTML = "";
 }
